459122938c0e5db027e79ec1d88a804769e75a47
kevincevallos <kevin_cevallos28@hotmail.es>
2020-04-13 05:49:03 -0500

    updateTablaUsuarios

--- controles/crudUsuarios.js
+++ controles/crudUsuarios.js
@@ -2,7 +2,7 @@
 let config = require('../knexfile')
 let env = 'development'
 let db = require('knex')(config[env])
-let bcrypt=require('bcrypt')
+let bcrypt = require('bcrypt')
 //CRUD Usuarios
 let leerUsuarios = (req, res) => {
     //console.log('GetUsuarios')
@@ -19,31 +19,77 @@ let leerUsuarios = (req, res) => {
         })
 }
 let login = (request, response) => {
-    console.log('LOGIN',request.body.clave)
-    user=request.body.correo,
-    password=request.body.clave,
-    db.select('*').from('users')
-    .where({ email: request.body.correo,
-            user_name: request.body.clave })
-        .then(registros => { 
-            if (user==registros[0].email&&password==registros[0].user_name) {
-                console.log('Hola',user,registros[0].user_name);
-                return response.status(200).json(
-                registros
-            )
-            }     
-        })
-        .catch(error => {
-            return response.status(404).json({
-                datos: error
+    console.log('LOGIN', request.body.clave)
+    user = request.body.correo,
+        password = request.body.clave,
+        db.select('*').from('users')
+            .where({
+                email: request.body.correo,
+                user_name: request.body.clave
             })
-        })
-  }
+            .then(registros => {
+                if (user == registros[0].email && password == registros[0].user_name) {
+                    console.log('Hola', user, registros[0].user_name);
+                    return response.status(200).json(
+                        registros
+                    )
+                }
+            })
+            .catch(error => {
+                return response.status(404).json({
+                    datos: error
+                })
+            })
+}
+//Find by Id
+let findById = (request, response) => {
+    //console.log('FindById_:', request.body.id)
+    id = request.body.id,
+        //password = request.body.clave,
+        db.select('*').from('carrera_user')
+            .where({ user_id: id })
+            .then(registros => {
+                if (id == registros[0].user_id) {
+                    console.log('Existe_:', id, registros[0].user_id, registros[0].carrera_id);
+                    return response.status(200).json(
+                        registros
+                    )
+                }
+            })
+            .catch(error => {
+                return response.status(404).json({
+                    datos: error
+                })
+            })
+}
+let updateById = (request, response) => {
+        id = request.body.id,
+        codigo = request.body.codigoUser,
+        console.log('USER_ID_:', id),
+        console.log('USER_CODE_:', codigo),
+        db.select('*').from('users')
+            .where({id: id})
+            .update({codigo_user: codigo})
+            .then(registros => {
+                return response.status(200).json(
+                    registros
+                )
+                /* if (id == registros[0].user_id) {
+                    console.log('Existe_:', id, registros[0].user_id ,registros[0].carrera_id);
+                    
+                }  */
+            })
+            .catch(error => {
+                return response.status(404).json({
+                    datos: error
+                })
+            })
+}
 
 let leerDocentes = (req, res) => {
 
     db.select('*').from('users')
-    .where({ role_id:"7" })
+        .where({ role_id: "7" })
         .then(registros => {
             return res.status(200).json(
                 registros
@@ -60,8 +106,8 @@ let ingresarUsuario = (req, res) => {
     console.log('REGISTRAR!!')
     let campos = req.body.campos
     let tabla = 'usuario'
-            db(tabla).insert(campos)
-            .then(resultado => {
+    db(tabla).insert(campos)
+        .then(resultado => {
             return res.status(200)
                 .json({ mensaje: 'usuario registrado' })
 
@@ -73,8 +119,8 @@ let ingresarUsuario = (req, res) => {
             })
         })
 }
-    
-let leerUsuarios = (req, res) => {
+
+/* let leerUsuarios = (req, res) => {
 
     db.select('*').from('usuarios')
         .then(registros => {
@@ -87,7 +133,7 @@ let leerUsuarios = (req, res) => {
                 datos: error
             })
         })
-}
+} */
 let leerRoles = (req, res) => {
 
     db.select('*').from('roles')
@@ -130,6 +176,43 @@ let leerCarreras = (req, res) => {
             })
         })
 }
+let leerCarrerasxUsuario = (req, res) => {
+
+    db.select('*').from('carrera_user')
+        .then(registros => {
+            return res.status(200).json(
+                registros
+            )
+        })
+        .catch(error => {
+            return res.status(404).json({
+                datos: error
+            })
+        })
+}
+
+/* let leerCarrerasxUsuario = (req, res) => {
+
+    db.select('carreras.nombre', 'users.name')
+    .from('carreras')
+    .join('carrera_user', 'carrera.id', '=', 'carrera_user.carrera_id')
+    .join('users', 'users.id', '=', 'carrera_user.user_id')
+
+    //select('*').from('carreras_user')
+        .then(registros => {
+            console.log('registros_:',registros)
+            return res.status(200).json(
+                registros
+            )
+        })
+        .catch(error => {
+            console.log('Error:',error)
+
+            return res.status(404).json({
+                datos: error
+            })
+        })
+} */
 
 
 let leerTabla = (req, res) => {
@@ -138,15 +221,16 @@ let leerTabla = (req, res) => {
     console.log(campos)
     console.log(tabla)
     db.select(campos).from(tabla)
-    .then(registros =>{
-        return res.status(200).json({
-            datos: registros
+        .then(registros => {
+            return res.status(200).json({
+                datos: registros
+            })
         })
-    })
-    .catch(error => {
-        return res.status(404).json({
-            datos: error        })
-    })
+        .catch(error => {
+            return res.status(404).json({
+                datos: error
+            })
+        })
 }
 
 let leerTablaId = (req, res) => {
@@ -154,34 +238,34 @@ let leerTablaId = (req, res) => {
     let tabla = req.body.tabla
     let condicion = req.body.condicion
     db(tabla).where(condicion).select(campos)
-    .then(registros => {
-        return res.status(200).json({
-            datos: registros
+        .then(registros => {
+            return res.status(200).json({
+                datos: registros
+            })
         })
-    })
-    .catch(error => {
-        return res.status(404).json({
-            datos: error
+        .catch(error => {
+            return res.status(404).json({
+                datos: error
+            })
         })
-    })
 }
 
-let ingresarRegistro = (req,res) => {
+let ingresarRegistro = (req, res) => {
     let campos = req.body.campos
     let tabla = req.body.tabla
     console.log(campos.primerNombre)
     console.log(tabla)
     db(tabla).insert(campos)
-    .then(registro => {
-        return res.status(200).json({
-            mensaje: "el registro due guardado"
+        .then(registro => {
+            return res.status(200).json({
+                mensaje: "el registro due guardado"
+            })
         })
-    })
-    .catch(error => {
-        return res.status(404).json({
-            mensaje: error
+        .catch(error => {
+            return res.status(404).json({
+                mensaje: error
+            })
         })
-    })
 }
 
 
@@ -190,32 +274,32 @@ let modificarRegistro = (req, res) => {
     let tabla = req.body.tabla
     let condicion = req.body.condicion
     db(tabla).where(condicion[0], condicion[1], condicion[2]).update(campos)
-    .then(registro => {
-        return res.status(200).json({
-            mensaje: "El registro fue modificado"
+        .then(registro => {
+            return res.status(200).json({
+                mensaje: "El registro fue modificado"
+            })
+
         })
-        
-    })
-    .catch(error => {
-        return res.status(404).json({
-            mensaje: error
+        .catch(error => {
+            return res.status(404).json({
+                mensaje: error
+            })
         })
-    })
 }
 
 
-
-
-
-
-
 module.exports = {
-   leerTabla,
-   leerTablaId,
-   leerUsuarios,
-   leerDocentes,
-   ingresarRegistro,
-   modificarRegistro,
-   ingresarUsuario,
-   login
+    leerRoles,
+    leerInstitutos,
+    leerCarreras,
+    leerTablaId,
+    leerUsuarios,
+    leerDocentes,
+    ingresarRegistro,
+    modificarRegistro,
+    ingresarUsuario,
+    login,
+    findById,
+    updateById,
+    leerCarrerasxUsuario
 }
--- rutas/ruta.js
+++ rutas/ruta.js
@@ -7,14 +7,20 @@ let api = express.Router()
   pdfControl = require('../controles/crudPdf')
   let multipartPdf = multipart()
 
-  api.post('/registro',usuariosControl.ingresarUsuario)
-  api.get('/prueba', pruebaControl.prueba)
+  //api.post('/registro',usuariosControl.ingresarUsuario)
+  //api.get('/prueba', pruebaControl.prueba)
   api.get('/leerUsuarios', usuariosControl.leerUsuarios)
   api.get('/leerDocentes', usuariosControl.leerDocentes)
+  api.get('/leerCarreras', usuariosControl.leerCarreras)
+  api.get('/leerInstitutos', usuariosControl.leerInstitutos)
+  api.get('/leerRoles', usuariosControl.leerRoles)
+  api.get('/leerCarrerasxUsuario', usuariosControl.leerCarrerasxUsuario)
   api.post('/login', usuariosControl.login)
+  api.post('/findById', usuariosControl.findById)
+  api.post('/updateById', usuariosControl.updateById)
   api.post('/subir-pdf', multipartPdf ,pdfControl.subirPdf)
   api.get('/ver-pdf/:id', pdfControl.verPdf)
-  api.get('/leerInstituto', usuariosControl.leerTabla)
+  //api.get('/leerInstituto', usuariosControl.leerTabla)
   
 
 module.exports = api
