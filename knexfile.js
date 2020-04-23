5b85991c7d5db839f7262c65b5750884f14795f4
Rick MY <ricky081297@hotmail.com>
2020-04-23 16:18:47 -0500

    cambios en app.js

--- knexfile.js
+++ knexfile.js
@@ -5,13 +5,7 @@ module.exports = {
         migrations: { tableName: 'knex_migrations'},
         seeds: { tableName: './seeds' },
         client: 'pg',
-        connection: {
-                        host: 'localhost', 
-                        port :'5433',
-                        user: 'postgres', 
-                        password: 'Rick08my12', 
-                        database: 'GEDI'
-                    }   
+        connection: 'postgres://fsgeznvynosdto:79ecd2dcecd6f34e5df7936b26453c6fc165868dcd36a7fa713fde41521327fa@ec2-18-206-84-251.compute-1.amazonaws.com:5432/dd0paks30un424'   
     },
     production: {
         migrations: { tableName: 'knex_migrations'},
