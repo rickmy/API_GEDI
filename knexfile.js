7c3b1bc598d4910345d939907a0a779907ced501
Rick MY <ricky081297@hotmail.com>
2020-04-23 16:08:31 -0500

    cambios en app.js

--- knexfile.js
+++ knexfile.js
@@ -7,10 +7,10 @@ module.exports = {
         client: 'pg',
         connection: {
                         host: 'localhost', 
-                        port :'5432',
-                        user: 'uxteqgfeiclzfw', 
-                        password: 'b67ce2a53f4d49babe9dba76df45b6d6c4ef13d1bae9ac6a8335c4902b100d4b', 
-                        database: 'dfsj6rb0gu2t1j'
+                        port :'5433',
+                        user: 'postgres', 
+                        password: 'Rick08my12', 
+                        database: 'GEDI'
                     }   
     },
     production: {
