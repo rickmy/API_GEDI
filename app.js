457014431d5d94209ec69c7e758f54e9d5822b68
Rick MY <ricky081297@hotmail.com>
2020-04-23 16:03:47 -0500

    cambios en app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express'),
   multipart = require('connect-multiparty')
 
 let app = express(),
-  rutas = require('../rutas/ruta')
+  rutas = require('./rutas/ruta')
 
 
 app.use(bodyParser.urlencoded({extended:true}))
