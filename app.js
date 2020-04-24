cb683fa87b931cf2f41307f63e0a1d90e17fffeb
Rick MY <ricky081297@hotmail.com>
2020-04-24 13:41:47 -0500

    version final

--- app.js
+++ app.js
@@ -12,15 +12,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
 
 app.use(multipart({uploadDir:'./pdfDirectorio'}))
-/* app.use(cors()) */
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'XMLHttpRequest','Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method')
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
-  res.header('Allow', 'GET, POST, PUT, DELETE')
-  next()
-})
+app.use(cors())
 
 app.use('/server', rutas)
 
